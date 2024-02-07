cd71e9cdf9a4694e16c3cc324bb122517f4132a5
Khoi Hoang <khoii87@gmail.com>
2024-02-07 23:51:29 +0100

    update: ✨ implemented form with new test for sign up form

--- src/views/sign-up/SignUp.spec.ts
+++ src/views/sign-up/SignUp.spec.ts
@@ -11,4 +11,72 @@ describe('sign up', () => {
 
     expect(header).toBeInTheDocument();
   });
+
+  it('has username input', () => {
+    render(SignUp);
+
+    const NodeUsernameLabel = screen.getByLabelText('Username');
+
+    expect(NodeUsernameLabel).toBeInTheDocument();
+  });
+
+  it('has email input', () => {
+    render(SignUp);
+
+    const nodeEmailLabel = screen.getByLabelText('E-mail');
+
+    expect(nodeEmailLabel).toBeInTheDocument();
+  });
+
+  describe('password fields', () => {
+    it('has password input', () => {
+      render(SignUp);
+
+      const nodePasswordLabel = screen.getByLabelText('Password');
+
+      expect(nodePasswordLabel).toBeInTheDocument();
+    });
+
+    it('has password type for password input', () => {
+      render(SignUp);
+
+      const nodePasswordInputType = screen.getByLabelText('Password');
+
+      expect(nodePasswordInputType).toHaveAttribute('type', 'password');
+    });
+
+    it('has password repeated input', () => {
+      render(SignUp);
+
+      const nodePasswordLabel = screen.getByLabelText('Password Repeat');
+
+      expect(nodePasswordLabel).toBeInTheDocument();
+    });
+
+    it('has password type for password repeat input', () => {
+      render(SignUp);
+
+      const nodePasswordInputType = screen.getByLabelText('Password Repeat');
+
+      expect(nodePasswordInputType).toHaveAttribute('type', 'password');
+    });
+  });
+
+  describe('button', () => {
+    it('has sign up button', () => {
+      render(SignUp);
+
+      const nodeButton = screen.getByRole('button', { name: 'Sign up' });
+
+      expect(nodeButton).toBeInTheDocument();
+    });
+
+    it('disables the button initially', () => {
+      render(SignUp);
+
+      const nodeButtonDisabled = screen.getByRole('button', { name: 'Sign up' });
+
+      expect(nodeButtonDisabled).toBeDisabled();
+    });
+  });
 });
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -2,6 +2,28 @@
 
 <template>
   <h1>Sign Up</h1>
+
+  <div>
+    <label for="username">Username</label>
+    <input type="text" id="username" />
+  </div>
+
+  <div>
+    <label for="email">E-mail</label>
+    <input type="text" id="email" />
+  </div>
+
+  <div>
+    <label for="password">Password</label>
+    <input type="password" id="password" />
+  </div>
+
+  <div>
+    <label for="password">Password Repeat</label>
+    <input type="password" id="password" />
+  </div>
+
+  <button disabled>Sign up</button>
 </template>
 
 <style></style>
