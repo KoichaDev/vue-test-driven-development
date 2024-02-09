ac883f86a6d1dc92d8d98baa81094454ea3267dc
Khoi Hoang <khoii87@gmail.com>
2024-02-09 03:06:12 +0100

    add: 💄 bootstrap design

--- frontend/src/views/sign-up/SignUp.vue
+++ frontend/src/views/sign-up/SignUp.vue
@@ -25,27 +25,39 @@ function handleSubmitButton() {
 </script>
 
 <template>
-  <h1>Sign Up</h1>
 
-  <form>
-    <div>
-      <label for="username">Username</label>
-      <input type="text" id="username" v-model="signupForm.username" />
-    </div>
-    <div>
-      <label for="email">E-mail</label>
-      <input type="text" id="email" v-model="signupForm.email" />
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input type="password" id="password" v-model="signupForm.password" />
-    </div>
-    <div>
-      <label for="password-repeat">Password Repeat</label>
-      <input type="password" id="password-repeat" v-model="signupForm.repeatedPassword" />
-    </div>
-    <button @click.prevent="handleSubmitButton" :disabled="isButtonDisabled">Sign up</button>
-  </form>
+
+  <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+    <h1>Sign Up</h1>
+    <form class="card" @submit.prevent="handleSubmitButton">
+      <div class="card-body">
+        <div class="mb-3">
+          <label class="form-label" for="username">Username</label>
+          <input type="text" id="username" class="form-control" v-model="signupForm.username" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="email">E-mail</label>
+          <input type="text" id="email" class="form-control" v-model="signupForm.email" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="password">Password</label>
+          <input type="password" id="password" class="form-control" v-model="signupForm.password" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="password-repeat">Password Repeat</label>
+          <input
+            type="password"
+            id="password-repeat"
+            class="form-control"
+            v-model="signupForm.repeatedPassword"
+          />
+        </div>
+        <div class="text-center">
+          <button class="btn btn-primary" :disabled="isButtonDisabled">Sign up</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style></style>
