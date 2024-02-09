2b51d22145d64eb5c6142a0ad8b872bcd51eae84
Khoi Hoang <khoii87@gmail.com>
2024-02-09 02:40:03 +0100

    add: ✨ proxy to the back-end server using Vite

--- frontend/src/views/sign-up/SignUp.vue
+++ frontend/src/views/sign-up/SignUp.vue
@@ -27,27 +27,25 @@ function handleSubmitButton() {
 <template>
   <h1>Sign Up</h1>
 
-  <div>
-    <label for="username">Username</label>
-    <input type="text" id="username" v-model="signupForm.username" />
-  </div>
-
-  <div>
-    <label for="email">E-mail</label>
-    <input type="text" id="email" v-model="signupForm.email" />
-  </div>
-
-  <div>
-    <label for="password">Password</label>
-    <input type="password" id="password" v-model="signupForm.password" />
-  </div>
-
-  <div>
-    <label for="password-repeat">Password Repeat</label>
-    <input type="password" id="password-repeat" v-model="signupForm.repeatedPassword" />
-  </div>
-
-  <button @click="handleSubmitButton" :disabled="isButtonDisabled">Sign up</button>
+  <form>
+    <div>
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="signupForm.username" />
+    </div>
+    <div>
+      <label for="email">E-mail</label>
+      <input type="text" id="email" v-model="signupForm.email" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="signupForm.password" />
+    </div>
+    <div>
+      <label for="password-repeat">Password Repeat</label>
+      <input type="password" id="password-repeat" v-model="signupForm.repeatedPassword" />
+    </div>
+    <button @click.prevent="handleSubmitButton" :disabled="isButtonDisabled">Sign up</button>
+  </form>
 </template>
 
 <style></style>
